93534cd86dfdcced2cdd43691715d1af5742213f
lana shotashvili <shotashvililana@gmail.com>
2026-03-16 18:39:24 +0400

    Add function updateCount to change count value

--- script.js
+++ script.js
@@ -13,19 +13,23 @@ incrementBtn.addEventListener("click", incrementCount);
 decrementBtn.addEventListener("click", decrementCount);
 resetBtn.addEventListener("click", resetCount);
 
+function updateCount() {
+  countElem.textContent = count;
+}
+
 function incrementCount() {
   count++;
-  countElem.textContent = count;
+  updateCount();
 }
 
 function decrementCount() {
   if (count > 0) {
     count--;
   }
-  countElem.textContent = count;
+  updateCount();
 }
 
 function resetCount() {
   count = 0;
-  countElem.textContent = count;
+  updateCount();
 }
